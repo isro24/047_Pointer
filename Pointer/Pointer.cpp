6a943ee02dff857917b75eb3b18c35f93430f132
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-10 18:02:06 +0700

    Menunjukkan penggunaan operator akses anggota objek mahasiswa

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -11,3 +11,18 @@ void mahasiswa::showNim() {	//implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };		//object mhs
+	mhs.showNim();			//Member Access Operator
+
+	mahasiswa& ref = mhs;	//Pointer Reference refMhs
+	ref.nim = 2;			//Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;	//Pointer Dereference pMhs
+	pMhs->nim = 3;			//Arrow Operator
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
