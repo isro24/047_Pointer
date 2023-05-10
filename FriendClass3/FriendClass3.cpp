8ea645ac06fe89821b0b054b924a0e49d5a50842
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-10 18:44:25 +0700

    Menambahkan implementasi kelas 'pelajar' dengan konstruktor default dan implementasi metode 'showNilaiPelajar()' di luar kelas 'manusia

--- FriendClass3/FriendClass3.cpp
+++ FriendClass3/FriendClass3.cpp
@@ -8,3 +8,17 @@ class manusia {
 public:
 	void showNilaiPelajar(pelajar&);
 };
+
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x) {
+	cout << "Nilai Pelajar: " << x.nilai;
+}
